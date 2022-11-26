c23d9171b6c46abc81784b13ee8d1eaeae16505f
Aliaksandr_Malakhau1 <altius200@gmail.com>
2022-11-26 22:30:24 +0100

    new project

---
+++ testCases/test_login.py
@@ -0,0 +1,38 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import pageObjects.LoginPage as lp
+
+
+
+
+
+class Test_001_Login:
+    baseURL = "https://admin-demo.nopcommerce.com/"
+    username = "admin@yourstore.com"
+    password = "admin"
+
+    def test_homePageTitle(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.baseURL)
+        act_title = self.driver.title
+        if act_title == "Your store. Login":
+            self.driver.close()
+            assert True
+        else:
+            assert False
+        
+    def test_login(self):
+        self.driver= webdriver.Chrome()
+        self.driver.get(self.baseURL)
+        self.lp = lp.LoginPage(self.driver)
+        self.lp.setUserName(self.username)
+        self.lp.setPassword(self.password)
+        self.lp.clickLogin()
+        act_title = self.driver.title
+        self.driver.close()
+        if act_title == "Dashboard / nopCommerce administration":
+            assert True
+        else:
+            assert False
